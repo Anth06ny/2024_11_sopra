1d1dfb6a0fab5f043d5d6109fdd2e5ee79977f7c
Anthony <anth06ny@gmail.com>
2024-11-28 11:17:44 +0100

    Fin TP Compose P1

--- app/src/main/java/com/amonteiro/a2024_11_sopra/MainActivity.kt
+++ app/src/main/java/com/amonteiro/a2024_11_sopra/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.amonteiro.a2024_11_sopra
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,12 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import com.amonteiro.a2024_11_sopra.ui.screens.SearchScreen
 import com.amonteiro.a2024_11_sopra.ui.theme._2024_11_sopraTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,36 +19,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             _2024_11_sopraTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "From onCreate",
+                    SearchScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!!!!",
-        modifier = modifier,
-        fontSize = 20.sp,
-    )
-}
-
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "Dark",
-    showSystemUi = true
-)
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun GreetingPreview() {
-    _2024_11_sopraTheme {
-        Greeting("From Preview")
     }
 }
---
+++ app/src/main/java/com/amonteiro/a2024_11_sopra/ui/screens/SearchScreen.kt
@@ -0,0 +1,48 @@
+package com.amonteiro.a2024_11_sopra.ui.screens
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.amonteiro.a2024_11_sopra.ui.theme._2024_11_sopraTheme
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun SearchScreenPreview() {
+    //Il faut remplacer NomVotreAppliTheme par le thème de votre application
+    //Utilisé par exemple dans MainActivity.kt sous setContent {...}
+    _2024_11_sopraTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            SearchScreen(modifier = Modifier.padding(innerPadding))
+       }
+    }
+}
+
+@Composable
+fun SearchScreen(modifier: Modifier = Modifier) {
+    Column (modifier= modifier) {
+        println("SearchScreen()")
+        Text(text = "Text1",fontSize = 20.sp)
+        Spacer(Modifier.size(8.dp))
+        Text(text = "Text2",fontSize = 14.sp)
+        PictureRowItem("Hello")
+        PictureRowItem("From")
+        PictureRowItem("PictureRowItem")
+    }
+}
+
+@Composable
+fun PictureRowItem(text : String){
+    Text(text = text,fontSize = 20.sp)
+}
